b2197d5383a0bda853c49df20d305d0b31a36b4c
tdinu <tdinu78@gmail.com>
2017-10-14 07:21:01 +0300

    Primefaces

--- src/main/java/beans/LoginBean.java
+++ src/main/java/beans/LoginBean.java
@@ -5,6 +5,8 @@ import models.Local;
 import utils.DatabaseInit;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -24,33 +26,45 @@ public class LoginBean implements Serializable {
     private static final long serialVersionUID = 1L;
     private String password;
     private String message, uname;
+    private Local selLocal;
+    private List<Local> selLocals;
 
+
+    public List<Local> getSelLocals() {
+        Local localEn=new Local(1,"English","en.png");
+        Local localRo=new Local(2,"Romana","ro.png");
+        selLocals = new ArrayList<Local>();
+        selLocals.add(localEn);
+        selLocals.add(localRo);
+        return selLocals;
+    }
+    public void setSelLocals(List<Local> selLocals) {
+        this.selLocals = selLocals;
+    }
+    public Local getSelLocal() {
+        return selLocal;
+    }
+    public void setSelLocal(Local selLocal) {
+        this.selLocal = selLocal;
+    }
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getUname() {
         return uname;
     }
-
     public void setUname(String uname) {
         this.uname = uname;
     }
-
-    Local localen=new Local(1,"en","English");
-    Local localRo=new Local(1,"ro","Romana");
 
     public String loginProject() {
         DatabaseInit a = new DatabaseInit();
